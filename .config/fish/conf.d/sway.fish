bae531578b09aa9f58eae6d07d51324ee5cb8971
avimuser <174873767+avimuser@users.noreply.github.com>
2025-07-19 10:43:56 +0530

    Move to WSL :(

--- .config/fish/conf.d/sway.fish
+++
@@ -1,4 +0,0 @@
-if test (tty) = "/dev/tty1"
-    export XDG_CURRENT_DESKTOP sway
-    exec sway
-end
--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -x 
-
-dir=$(dirname $(readlink -f $0)) 
-
-mkdir -p $HOME/.config
-stow -d $dir -t $HOME .
